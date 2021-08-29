e606e3846c466b62f77aa92501fbd9669ac8bfa2
sjnp <supawet.jnp@gmail.com>
2021-08-30 01:01:36 +0700

    complete html and css | finish starting state in app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-const handState = ["✊", "✋", "✌️"]
+const startGame = () => {
+    const handEmojis = ["✊", "✋", "✌️"];
+    const gamePanel = document.getElementById("game-panel");
+
+    const startButton = document.getElementById("start-btn");
+    const buttonElements = [startButton];
+
+    // Create emoji buttons
+    for (let i = 0; i < handEmojis.length; i ++) {
+        const newButton = document.createElement("button");
+        newButton.setAttribute("type", "submit");
+        newButton.setAttribute("onclick", "play()");
+        newButton.setAttribute("value", handEmojis[i]);
+        newButton.innerText = handEmojis[i];
+        buttonElements.push(newButton); 
+    }
+
+    const renderGame = () => { 
+        gamePanel.innerHTML = "";
+        for (let i = 1; i < buttonElements.length; i++) {
+            gamePanel.appendChild(buttonElements[i]);
+        }
+    }
+
+    const renderStart = () => {
+        gamePanel.innerHTML = "";
+        gamePanel.appendChild(buttonElements[0]);
+    }
+   
+    // render round
+};
